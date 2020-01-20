50ca37a46376a5f2021587829908ae9e3c2a310c
David <dcornish84@gmail.com>
2020-01-20 16:12:13 -0600

    first commit

---
+++ EFWorkforce/Data/ApplicationDbContext.cs
@@ -0,0 +1,22 @@
+﻿using EFWorkforce.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EFWorkforce.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+
+        public DbSet<Employee> Employee { get; set; }
+
+        //reflect our tables in our database, has to be called the same thing as database table
+
+        public DbSet<Department> Department { get; set; }
+
+        public DbSet<Computer> Computer { get; set; }
+    }
+}
